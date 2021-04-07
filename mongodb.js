268f136ff6a95faef20e4ab0b09f47c0b39ec7e5
Glitch (hello-sqlite) <none>
2021-04-07 23:26:26 +0000

    🐥🍅 Checkpoint ./mongodb.js:18235330/11429

--- mongodb.js
+++ mongodb.js
@@ -1,39 +1,58 @@
-function dbQuery(query) {
+async function dbInsert(coll, data) {
+  
+  var database = await dbOpen();
+  database["dbo"].collection(coll).insertOne(data, function(err, res) {
+    if (err) throw err;
+    console.log("1 document inserted");
+    database["db"].close();
+  });
+}
+
+async function dbSelect(coll, data) {
+  var database = await dbOpen();
+  database["dbo"]
+    .collection(coll)
+    .find(data)
+    .toArray((err, res) => {
+      if (err) throw err;
+      console.log("1 document inserted");
+      database["db"].close();
+    });
+}
+
+function dbOpen() {
   return new Promise((res, rej) => {
     var { MongoClient } = require("mongodb"),
       url = "mongodb://localhost:27017/";
-
     MongoClient.connect(url, function(err, db) {
       if (err) throw err;
-      var dbo = db.db("mydb");
-      dbo
-        .collection("games")
-        .find(query)
-        .toArray((err, result) => {
-          if (err) throw err;
-          res(result);
-          db.close();
-        });
+      var dbo = db.db("gamev1");
+      res({ db: db, dbo: dbo });
     });
   });
 }
 
-function dbInsert(query) {
-  return new Promise((res, rej) => {
-    var { MongoClient } = require("mongodb"),
-      url = "mongodb://localhost:27017/";
+async function dbUpdate() {
+  
+}
 
-    MongoClient.connect(url, function(err, db) {
+/*(function() {
+  var MongoClient = require("mongodb").MongoClient;
+  var url = "mongodb://localhost:27017/";
+
+  MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("gamev1");
+    dbo.createCollection("users", function(err, res) {
       if (err) throw err;
-      var dbo = db.db("mydb");
-      dbo
-        .collection("games")
-        .insert(query)
-        .toArray((err, result) => {
-          if (err) throw err;
-          res(result);
-          db.close();
-        });
+      console.log("Collection created!");
+      db.close();
     });
   });
+})();
+*/
+
+module.exports = {
+  dbSelect: dbSelect,
+  dbInsert: dbInsert
 }
