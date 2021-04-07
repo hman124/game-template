6074b3b68cab1f2c97a9cdc8eb804c386a865de2
Glitch (hello-sqlite) <none>
2021-04-07 18:20:58 +0000

    📚💆 Checkpoint ./mongodb.js:18235330/2474

--- mongodb.js
+++ mongodb.js
@@ -1,18 +1,39 @@
-function query() {
-  var { MongoClient } = require("mongodb"),
-    url = "mongodb://localhost:27017/";
+function dbQuery(query) {
+  return new Promise((res, rej) => {
+    var { MongoClient } = require("mongodb"),
+      url = "mongodb://localhost:27017/";
 
-  MongoClient.connect(url, function(err, db) {
-    if (err) throw err;
-    var dbo = db.db("mydb");
-    var query = { address: /^S/ };
-    dbo
-      .collection("customers")
-      .find(query)
-      .toArray((err, result) => {
-        if (err) throw err;
-        res(result);
-        db.close();
-      });
+    MongoClient.connect(url, function(err, db) {
+      if (err) throw err;
+      var dbo = db.db("mydb");
+      dbo
+        .collection("games")
+        .find(query)
+        .toArray((err, result) => {
+          if (err) throw err;
+          res(result);
+          db.close();
+        });
+    });
+  });
+}
+
+function dbInsert(query) {
+  return new Promise((res, rej) => {
+    var { MongoClient } = require("mongodb"),
+      url = "mongodb://localhost:27017/";
+
+    MongoClient.connect(url, function(err, db) {
+      if (err) throw err;
+      var dbo = db.db("mydb");
+      dbo
+        .collection("games")
+        .insert(query)
+        .toArray((err, result) => {
+          if (err) throw err;
+          res(result);
+          db.close();
+        });
+    });
   });
 }
