75ae1859ed4bce26cd4c56413c431c1195957c6c
Glitch (hello-sqlite) <none>
2021-04-08 17:56:45 +0000

    💂🐪 Checkpoint ./mongodb.js:18235330/7253 ./database.js:18235330/10

--- database.js
+++ database.js
@@ -1,78 +1,65 @@
-const dbFile = "./.data/gamev1.1.db";
-const fs = require("fs");
-const exists = fs.existsSync(dbFile);
-const sqlite3 = require("sqlite3").verbose();
-const db = new sqlite3.Database(dbFile);
-
-db.serialize(() => {
-  if (!exists) {
-    db.run(
-      "CREATE TABLE Games (gamePin TEXT, numUsers INT, hostId TEXT, isStarted BOOL)"
-    );
-    db.run(
-      "CREATE TABLE Users (userId TEXT, screenName TEXT, currentGame TEXT, isHost BOOL)"
-    );
-  } else {
-    //console.log("Database ready");
-  }
-});
-
-function dbAll(query, stmts) {
-  if (!stmts) {
-    stmts = [];
-  }
-  return new Promise((res, rej) => {
-    db.all(query, ...stmts, (err, rows) => {
-      if (err) {
-        rej(err);
-      } else {
-        res(rows);
-      }
+function dbInsert(coll, data) {
+  return new Promise(async (resolve, reject) => {
+    var database = await dbOpen();
+    database["coll"].insertOne(data, function(err, res) {
+      if (err) throw err;
+      resolve(res);
+      database["db"].close();
     });
   });
 }
 
-function dbRun(query, stmts) {
-  if (!stmts) {
-    stmts = [];
-  }
-  return new Promise((res, rej) => {
-    db.run(query, ...stmts, err => {
-      if (err) {
-        rej(err);
-      } else {
-        res();
-      }
+function dbSelect(coll, data) {
+  return new Promise(async (resolve, reject) => {
+    var database = await dbOpen();
+    database["coll"].find(data).toArray((err, res) => {
+      if (err) throw err;
+      resolve(res);
+      database["db"].close();
     });
   });
 }
 
-function dbFirst(query, stmts) {
-  if (!stmts) {
-    stmts = [];
-  }
-  return new Promise((res, rej) => {
-    db.all(query, ...stmts, (err, rows) => {
-      if (err) {
-        rej(err);
-      } else if (rows) {
-        if (rows.length) {
-          res(rows[0]);
-        } else {
-          res([]);
-        }
-      }
+function dbOpen(coll) {
+  return new Promise((resolve, reject) => {
+    var { MongoClient } = require("mongodb"),
+      url = "mongodb://localhost:27017/";
+    MongoClient.connect(url, function(err, db) {
+      if (err) throw err;
+      var dbo = db.db("gamev1");
+      resolve({ db: db, dbo: dbo, coll: dbo.collection(coll) });
     });
   });
 }
 
-async function dbList() {
-  return await dbAll("Select * From Users", []);
+function dbUpdate(coll, query, set) {
+  return new Promise(async (resolve, reject) => {
+    var database = await dbOpen(coll);
+    database["coll"].updateOne(query, set, (err, res) => {
+      if (err) throw err;
+      resolve(res);
+      database["db"].close();
+    });
+  });
 }
+
+function dbSetup() {
+  var MongoClient = require("mongodb").MongoClient;
+  var url = "mongodb://localhost:27017/";
+  MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("gamev1");
+    dbo.createCollection("users", function(err, res) {
+      if (err) throw err;
+      console.log("Collection created!");
+      db.close();
+    });
+  });
+}
+dbSetup()
 
 module.exports = {
-  run: dbRun,
-  all: dbAll,
-  first: dbFirst,
-  list: dbList
+  dbSelect: dbSelect,
+  dbInsert: dbInsert,
+  dbUpdate: dbUpdate
 };
