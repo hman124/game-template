5d3fbf13b16ddb04b83551f3f050b3c0f40b88b5
Glitch (hello-sqlite) <none>
2021-04-10 02:34:20 +0000

    👶🎲 Checkpoint ./views/host/wait.html:18235330/866 ./sockets.js:18235330/579 ./views/player/wait.html:18235330/126

--- sockets.js
+++ sockets.js
@@ -10,5 +10,14 @@ module.exports = function(http) {
       socket.join(data[0]);
       socket.join(data[1]);
     });
+    socket.on("startGame", async data => {
+      const game = await users.getGame(data['gamePin']);
+      if(game && game.hostId == data['auth']) {
+        await db.run("Update Games Set (isStarted=1) Where gamePin=?", [data['gamePin']]);
+        io.to(data['gamePin']).emit("gameStartSuccess");
+      } else {
+        io.to(data['auth']).emit("gameStartFailure");
+      }
+    });
   });
 };
