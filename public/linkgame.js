2f80bc8a125a059ffbb820b0e93a4c441f595a22
Glitch (hello-sqlite) <none>
2021-05-22 00:34:07 +0000

    ⛵️👕 Checkpoint ./public/linkgame.js:18235330/400

--- public/linkgame.js
+++ public/linkgame.js
@@ -9,11 +9,12 @@ function linkGame(gamePin, userId) {
   });
 }
 
- window.addEventListener("click", addUnload);
 var addUnload = () => {
+  console.log("added");
    window.addEventListener('beforeunload', function(e) {
     e.preventDefault();
     e.returnValue = '';
    });
   window.removeEventListener("click", addUnload);
 }
+ window.addEventListener("click", addUnload);
