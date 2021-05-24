c0f58f5df6070416776d7eded9add2e1e47a327c
Glitch (hello-sqlite) <none>
2021-05-24 21:04:44 +0000

    🐡👲 Checkpoint ./sockets.js:18235330/317

--- sockets.js
+++ sockets.js
@@ -6,15 +6,15 @@ module.exports = function(http) {
 
   io.on("connect", socket => {
     socket.on("linkGame", data => {
+      data.forEach(x => {
+        socket.join(x);
+      });
       io.to(data[0]).emit("newUser");
-      socket.join(data[0]);
-      socket.join(data[1]);
     });
     
     socket.on("win", async userId => {
       var user = await db.first("Select * From Users Where userId=?", userId),
           game = await db.first("Select * From Games Where gamePin=?", user.currentGame);
-        console.log(game.hostId);
         io.to(game.hostId).emit("win", user);
     });
     
@@ -29,4 +29,5 @@ module.exports = function(http) {
       }
     });
   });
+  return io;
 };
